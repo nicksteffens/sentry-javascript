498e8f35d450b3917f869f6b9f3255f560a41660
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-08-30 10:46:57 +0200

    More logging in the handler

--- packages/nextjs/src/utils/withSentry.ts
+++ packages/nextjs/src/utils/withSentry.ts
@@ -1,5 +1,5 @@
 import { captureException, flush, Handlers, withScope } from '@sentry/node';
-import { addExceptionMechanism } from '@sentry/utils';
+import { addExceptionMechanism, logger } from '@sentry/utils';
 import { NextApiHandler } from 'next';
 
 const { parseRequest } = Handlers;
@@ -10,9 +10,11 @@ type WrappedNextApiHandler = NextApiHandler;
 export const withSentry = (handler: NextApiHandler): WrappedNextApiHandler => {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   return async (req, res) => {
+    logger.log(`withSentry handler called...`);
     try {
       return await handler(req, res); // Call Handler
     } catch (e) {
+      logger.log('Error catched in the handler: ', e.message);
       withScope(scope => {
         scope.addEventProcessor(event => {
           addExceptionMechanism(event, {
